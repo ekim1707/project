59a16d0dc2a1a6243e64f2829e238c7e6622c364
ekim1707 <ekim1707@gmail.com>
2023-07-20 00:47:48 -0400

    added semantic-ui-react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import ApplicationRouter from "./routes";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
